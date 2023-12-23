d3bd38a513945a0411b28394cc89d9b49636cd7d
Evgeniy Filimonov <fil.eugene@gmail.com>
2023-12-23 22:06:04 +0200

    Puzzle17 part1 done with min-priority queue

--- puzzle17-1.py
+++ puzzle17-1.py
@@ -1,4 +1,5 @@
 import sys
+from heapq import heappush, heappop
 
 grid = []
 for line in sys.stdin:
@@ -7,15 +8,37 @@ for line in sys.stdin:
 h, w = len(grid), len(grid[0])
 print('h', h, 'w', w)
 
-q = {((0, 0), (0, 0), 0): (0, None)}
-done = {}
-while q:
-    key = min(q, key=lambda x: q[x][0])
-    if key[0] == (w - 1, h - 1):
-        break
-    dist, _ = done[key] = q.pop(key)
+def addq(prio, key):
+    e = [prio, key]
+    d[key] = e
+    heappush(q, e)
 
-    (x, y), (x0, y0), steps = key
+def modq(prio, e):
+    key = e[1]
+    e[1] = () # mark unused
+    addq(prio, key)
+
+def popq():
+    while q:
+        prio, key = heappop(q)
+        if key != ():
+            del d[key]
+            return prio, key
+    return None
+
+q = []
+d = {}
+addq(0, (0, 0, 0, 0, 0))
+done = {}
+while True:
+    e = popq()
+    assert(e)
+    dist, key = e
+    done[key] = 1
+    x, y, x0, y0, steps = key
+    if (x, y) == (w - 1, h - 1):
+        break
+    
     dx, dy = x - x0, y - y0
     for dx1, dy1 in ((-1, 0), (0, -1), (1, 0), (0, 1)):
         x1, y1 = x + dx1, y + dy1
@@ -29,16 +52,18 @@ while q:
             steps1 = steps + 1
         else:
             steps1 = 1
-        key1 = ((x1, y1), (x, y), steps1)
+        key1 = (x1, y1, x, y, steps1)
         if key1 in done:
             continue
 
         dist1 = dist + grid[y1][x1]
-        v1 = q.get(key1, None)
-        if v1 == None or dist1 < v1[0]:
-            q[key1] = (dist1, (x, y))
+        e1 = d.get(key1, None)
+        if e1 == None:
+            addq(dist1, key1)
+        elif dist1 < e1[0]:
+            modq(dist1, e1)
 
     # print('done', done)
     # print('q', q)
     # print()
-print(q[key])
+print(dist)
