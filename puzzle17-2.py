7af30b4008c80d3fb2522f309eb884e3d75b1096
Evgeniy Filimonov <fil.eugene@gmail.com>
2023-12-23 23:08:45 +0200

    Puzzle17 part2 done

---
+++ puzzle17-2.py
@@ -0,0 +1,73 @@
+import sys
+from heapq import heappush, heappop
+
+grid = []
+for line in sys.stdin:
+    line = line.rstrip()
+    grid.append([int(x) for x in line])
+h, w = len(grid), len(grid[0])
+print('h', h, 'w', w)
+
+def addq(prio, key):
+    e = [prio, key]
+    d[key] = e
+    heappush(q, e)
+
+def modq(prio, e):
+    key = e[1]
+    e[1] = () # mark unused
+    addq(prio, key)
+
+def popq():
+    while q:
+        prio, key = heappop(q)
+        if key != ():
+            del d[key]
+            return prio, key
+    return None
+
+q = []
+d = {}
+addq(0, (0, 0, -1, 0, 0))
+addq(0, (0, 0, 0, -1, 0))
+done = {}
+while True:
+    e = popq()
+    assert(e)
+    # print(e)
+    dist, key = e
+    done[key] = 1
+    x, y, x0, y0, steps = key
+    if (x, y) == (w - 1, h - 1) and steps >= 4:
+        break
+    
+    dx, dy = x - x0, y - y0
+    for dx1, dy1 in ((-1, 0), (0, -1), (1, 0), (0, 1)):
+        x1, y1 = x + dx1, y + dy1
+        if x1 not in range(0, w) or y1 not in range(0, h):
+            continue
+        if (x1, y1) == (x0, y0):
+            continue
+        if (dx1, dy1) == (dx, dy):
+            if steps == 10:
+                continue
+            steps1 = steps + 1
+        else:
+            if steps < 4:
+                continue
+            steps1 = 1
+        key1 = (x1, y1, x, y, steps1)
+        if key1 in done:
+            continue
+
+        dist1 = dist + grid[y1][x1]
+        e1 = d.get(key1, None)
+        if e1 == None:
+            addq(dist1, key1)
+        elif dist1 < e1[0]:
+            modq(dist1, e1)
+
+    # print('done', done)
+    # print('q', q)
+    # print()
+print(dist)
