5070cd1c40b876ab9e05d52728b635ca6b546afe
Evgeniy Filimonov <fil.eugene@gmail.com>
2024-02-08 22:51:40 +0200

    Restore puzzle9 solutions in Python

--- python/puzzle9-2.py
+++ python/puzzle9-2.py
@@ -3,14 +3,14 @@ import sys
 sum = 0
 for line in sys.stdin:
     hist = list(map(int, line.rstrip().split()))
-    last_vals = []
+    first_vals = []
     while not all([v == 0 for v in hist]):
-        last_vals.append(hist[-1])
+        first_vals.append(hist[0])
         hist = [nxt - prev for prev, nxt in zip(hist[:-1], hist[1:])]
 
     new_val = 0
-    for v in last_vals:
-        new_val += v
+    for v in reversed(first_vals):
+        new_val = v - new_val
     # print(new_val)
     sum += new_val
 print(sum)
