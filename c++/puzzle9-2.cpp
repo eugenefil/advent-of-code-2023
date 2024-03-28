70af12c0a3ff3c78e4ebfbb908e2bbc4a3018ea3
Evgeniy Filimonov <fil.eugene@gmail.com>
2024-03-28 13:23:01 +0200

    Puzzle9-2 done in C++

---
+++ c++/puzzle9-2.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+#include <string_view>
+#include <charconv>
+#include <cassert>
+#include <vector>
+#include <algorithm>
+
+int main()
+{
+    long sum = 0;
+    for (std::string line; std::getline(std::cin, line);) {
+        std::vector<long> vals;
+        std::string_view s(line);
+        assert(!s.empty());
+        while (!s.empty()) {
+            auto pos = s.find_first_not_of(' ');
+            assert(pos != s.npos);
+            s.remove_prefix(pos);
+
+            long val = 0;
+            auto [ptr, ec] = std::from_chars(s.data(), s.data() + s.size(), val);
+            assert(ec == std::errc{});
+            s.remove_prefix(ptr - s.data());
+            vals.push_back(val);
+        }
+
+        decltype(vals) first_vals;
+        auto is_zero = [](long n) { return n == 0; };
+        while (!std::all_of(vals.begin(), vals.end(), is_zero)) {
+            assert(vals.size() > 1);
+            first_vals.push_back(vals.front());
+            for (decltype(vals)::size_type i = 1; i < vals.size(); ++i) {
+                vals[i - 1] = vals[i] - vals[i - 1];
+            }
+            vals.pop_back();
+        }
+
+        std::reverse(first_vals.begin(), first_vals.end());
+        long extrap = 0;
+        for (auto v : first_vals)
+            extrap = v - extrap;
+
+        sum += extrap;
+    }
+    std::cout << sum << '\n';
+    return 0;
+}
