a66d2e334f4d0b3f4425addcc1ebb7bcd2121b9e
Evgeniy Filimonov <fil.eugene@gmail.com>
2024-08-31 19:53:16 +0200

    rust: Puzzle7-1 done

---
+++ rust/src/bin/puzzle7-1.rs
@@ -0,0 +1,119 @@
+#[derive(PartialEq, Eq, Hash, Debug)]
+struct Card {
+    label: char,
+}
+
+impl Card {
+    fn strength(&self) -> u8 {
+        match self.label {
+            '2' => 2,
+            '3' => 3,
+            '4' => 4,
+            '5' => 5,
+            '6' => 6,
+            '7' => 7,
+            '8' => 8,
+            '9' => 9,
+            'T' => 10,
+            'J' => 11,
+            'Q' => 12,
+            'K' => 13,
+            'A' => 14,
+            _ => panic!(),
+        }
+    }
+}
+
+use std::cmp::Ordering;
+
+impl Ord for Card {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.strength().cmp(&other.strength())
+    }
+}
+
+impl PartialOrd for Card {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+#[derive(PartialEq, Eq, PartialOrd, Ord)]
+enum HandType {
+    HighCard,
+    OnePair,
+    TwoPair,
+    ThreeOfAKind,
+    FullHouse,
+    FourOfAKind,
+    FiveOfAKind,
+}
+
+#[derive(PartialEq, Eq, PartialOrd, Ord)]
+struct Hand {
+    hand_type: HandType,
+    cards: [Card; 5],
+}
+
+impl Hand {
+    fn cards_to_type(cards: &[Card; 5]) -> HandType {
+        let mut counts = std::collections::HashMap::new();
+        cards
+            .iter()
+            .for_each(|c| *counts.entry(c).or_insert(0u32) += 1);
+        let groups = counts.values().map(|&c| c).collect::<Vec<u32>>();
+        match groups.len() {
+            1 => HandType::FiveOfAKind,
+            2 => match groups[0] {
+                1 | 4 => HandType::FourOfAKind,
+                _ => HandType::FullHouse,
+            },
+            3 => {
+                if groups[0] == 2 || groups[1] == 2 {
+                    HandType::TwoPair
+                } else {
+                    HandType::ThreeOfAKind
+                }
+            }
+            4 => HandType::OnePair,
+            _ => HandType::HighCard,
+        }
+    }
+}
+
+impl From<&str> for Hand {
+    fn from(cards: &str) -> Self {
+        let cards: [Card; 5] = cards
+            .chars()
+            .map(|label| Card { label })
+            .collect::<Vec<Card>>()
+            .try_into()
+            .unwrap();
+        let hand_type = Hand::cards_to_type(&cards);
+        Self { cards, hand_type }
+    }
+}
+
+#[derive(PartialEq, Eq, PartialOrd, Ord)]
+struct Bid {
+    hand: Hand,
+    amount: usize,
+}
+
+fn main() {
+    let mut bids = Vec::new();
+    for line in std::io::stdin().lines().map(|s| s.unwrap()) {
+        let (hand, amount) = line.split_once(' ').unwrap();
+        bids.push(Bid {
+            hand: hand.into(),
+            amount: amount.trim_end().parse::<usize>().unwrap(),
+        });
+    }
+    bids.sort();
+    println!("{}", bids
+        .into_iter()
+        .enumerate()
+        .map(|(rank, bid)| (rank + 1) * bid.amount)
+        .sum::<usize>()
+    );
+}
